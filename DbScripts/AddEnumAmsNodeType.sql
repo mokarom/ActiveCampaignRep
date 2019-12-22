5ab5b7e7f4f045cab223d0a5a89dde83dad8aad6
mokarom <mokarom@gmail.com>
2019-12-22 17:34:03 +0600

    Test CD

--- DbScripts/AddEnumAmsNodeType.sql
+++ DbScripts/AddEnumAmsNodeType.sql
@@ -1,3 +1,3 @@
-IF (SELECT COUNT(*) FROM [Global].[EnumAMSNodeTypes] WHERE [EnumAMSNodeType] = 30021) = 0
+IF (SELECT COUNT(*) FROM [Global].[EnumAMSNodeTypes] WHERE [EnumAMSNodeType] = 30022) = 0
 	INSERT INTO [Global].[EnumAMSNodeTypes] ([EnumAMSNodeType],[AMSNodeTypeName],[AMSNodeTypeDescription],[AMSNodeTypeResourceKeyId],[AMSNodeTypeCreatedDate])
-	VALUES ('30021','TestAmsNodetype','TestAmsNodetype', null, '2019/02/03')
+	VALUES ('30022','TestAmsNodetype','TestAmsNodetype', null, '2019/02/03')
